bdb817ce42c79b3f69469662371166af77f800bc
hyunwoo <bhw9102@gmail.com>
2018-12-25 20:42:03 +0900

    이름을 입력받아 플레이어를 생성하거나, 기존의 플레이어 정보를 반환하다.

--- session/constants.py
+++ session/constants.py
@@ -1,6 +1,7 @@
 IS_LOGINED = 'is_logined'
 
 CONST_PLAYER = 'player'
+CONST_PLAYER_ID = 'player_id'
 CONST_NAME = 'name'
 
 
--- session/views.py
+++ session/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from session.models import Player
 from session.forms import NameForm
 from session.constants import *
+from django.core import serializers
 
 
 def index(request):
@@ -17,19 +18,26 @@ def entrance(request):
 def login(request):
     if request.method == "POST":
         form = NameForm(request.POST)
+        print(request.POST)
         if form.is_valid():
-            player = Player.objects.filter(name=form.cleaned_data["name"]).first()
+            name = form.cleaned_data[CONST_NAME]
+            player = Player.objects.filter(name=name).first()
 
             if player is None:
                 # 등록된 플레이어가 없을 때
-                player = Player.objects.create(name=form.name)
+                player = Player.objects.create(name=name)
             request.session[IS_LOGINED] = True
-            request.session[CONST_PLAYER] = player
+            request.session[CONST_PLAYER_ID] = player.id
+            player_data = serializers.serialize('json', Player.objects.filter(pk=player.id), fields=('id', 'name'))
+            request.session[CONST_PLAYER] = player_data
             return HttpResponseRedirect(reverse('room-list'))
     # GET
-    player = request.session.get(CONST_PLAYER)
+    player_id = request.session.get(CONST_PLAYER_ID)
+    player_data = serializers.deserialize('json', request.session.get(CONST_PLAYER), ignorenonexistent=True)
+    print(player_data)
     form = NameForm(initial={'name': ''})
-    if player is not None:
+    if player_id is not None:
+        player = Player.objects.filter(pk=player_id).first()
         form = NameForm(initial={'name': player.name})
     return render(request, 'session/login.html', {'form': form})
 
