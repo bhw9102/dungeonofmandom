a5780a7d6ece519d2ee29fbe25369827029a14dc
hyunwoo <bhw9102@gmail.com>
2019-01-21 00:39:38 +0900

    뉴렐릭 관력 코드를 지우다.

--- dungeonofmandom/wsgi.py
+++ dungeonofmandom/wsgi.py
@@ -8,16 +8,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-import newrelic.agent
 from django.core.wsgi import get_wsgi_application
-import platform
-
-if platform.system() == 'linux':
-    newrelic.agent.initialize('/var/lib/jenkins/workspace/dungeonofmandom/newrelic.inios.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dungeonofmandom.settings')
 
 application = get_wsgi_application()
-
-if platform.system() == 'linux':
-    application = newrelic.agent.wsgi_application()(application)
